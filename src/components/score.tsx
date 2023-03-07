4240e4e8d2e0a620da5e40c660a578030d6fa9fe
Darshan <itsponikar@gmail.com>
2023-03-07 13:21:32 +0530

    fix: I think I have fixed the image loading issue

--- src/components/score.tsx
+++ src/components/score.tsx
@@ -1,41 +1,12 @@
 import { Image, SkImage, useImage } from "@shopify/react-native-skia";
-import React from "react";
+import React, { FC, memo } from "react";
 import { useGameOverEffect } from "../hooks/useGameOverEffect";
 import { useGameStateEffect } from "../hooks/useGameStateEffect";
 
-const useDigits = (digit: number) => {
-  const images: SkImage[] = [];
-
-  const zero = useImage(require("../assets/0.png"));
-  const one = useImage(require("../assets/1.png"));
-  const two = useImage(require("../assets/2.png"));
-  const three = useImage(require("../assets/3.png"));
-  const four = useImage(require("../assets/4.png"));
-  const five = useImage(require("../assets/5.png"));
-  const six = useImage(require("../assets/6.png"));
-  const seven = useImage(require("../assets/7.png"));
-  const eight = useImage(require("../assets/8.png"));
-  const nine = useImage(require("../assets/9.png"));
-
-  const digits = [zero, one, two, three, four, five, six, seven, eight, nine];
-
-  const isPending = digits.every((digit) => !digit);
-
-  if (isPending) return [];
-
-  const digitArray = digit.toString().split("");
-
-  digitArray.forEach((digit) => {
-    images.push(digits[parseInt(digit)]);
-  });
-
-  return images;
-};
+const CacheImages: Record<string, SkImage> = {};
 
 export const Score = () => {
   const [currentScore, setCurrentScore] = React.useState(0);
-
-  const digitsImage = useDigits(currentScore);
 
   useGameStateEffect(() => setCurrentScore((oldScore) => oldScore + 20));
 
@@ -47,21 +18,62 @@ export const Score = () => {
     });
   });
 
+  const digits = currentScore
+    .toString()
+    .split("")
+    .map((digit) => Number(digit));
+
   return (
     <>
-      {digitsImage.map((digit, index) => {
-        return (
-          <Image
-            key={index}
-            image={digit}
-            y={60}
-            x={(index + 0.5) * 20}
-            width={25}
-            height={25}
-            fit="contain"
-          />
-        );
+      {digits.map((digit, index) => {
+        if (CacheImages[digit]) {
+          return (
+            <Image
+              image={CacheImages[digit]}
+              y={60}
+              key={index}
+              x={(index + 0.5) * 20}
+              width={25}
+              height={25}
+              fit="contain"
+            />
+          );
+        }
+
+        return <Digit digit={digit} key={index} index={index} />;
       })}
     </>
   );
 };
+
+const DigitImages: Record<number, typeof require> = {
+  0: require("../assets/0.png"),
+  1: require("../assets/1.png"),
+  2: require("../assets/2.png"),
+  3: require("../assets/3.png"),
+  4: require("../assets/4.png"),
+  5: require("../assets/5.png"),
+  6: require("../assets/6.png"),
+  7: require("../assets/7.png"),
+  8: require("../assets/8.png"),
+  9: require("../assets/9.png"),
+};
+
+const Digit: FC<{ digit: number; index: number }> = memo(({ digit, index }) => {
+  const image = useImage(DigitImages[digit]);
+
+  if (!image) return null;
+
+  CacheImages[digit] = image;
+
+  return (
+    <Image
+      image={image}
+      y={60}
+      x={(index + 0.5) * 20}
+      width={25}
+      height={25}
+      fit="contain"
+    />
+  );
+});
